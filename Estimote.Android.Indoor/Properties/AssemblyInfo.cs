b99a72fc9beccf2ab9f6a1d9a27cb8876dd6e9dc
Piotr Krawiec <piotr@krawiec.me>
2019-04-11 11:56:19 +0200

    Bump Android Indoor & Proximity versions

--- Estimote.Android.Indoor/Properties/AssemblyInfo.cs
+++ Estimote.Android.Indoor/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.5.4.0")]
-[assembly: AssemblyInformationalVersion("2.5.4")]
+[assembly: AssemblyVersion("2.5.4.3")]
+[assembly: AssemblyInformationalVersion("2.5.4.3")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
